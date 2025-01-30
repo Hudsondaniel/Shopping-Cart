494864474e16d962a445426d5954e1d2133b8fbe
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2025-01-30 16:18:40 +0530

    make a image carousal for homepage

--- ShoppingCart/src/components/Navbar.jsx
+++ ShoppingCart/src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
+import '../CSS/navbar.css'
 
 const Navbar = () => {
-  return (
-    <nav className="flex ">
-      <h1 className="text-xl font-bold">My Shop</h1>
-      <div>
-        <Link to="/" className="mr-4">Home</Link>
-        <Link to="/shop">Shop</Link>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="nav-container ">
+            <h1 className="nav-heading">CartBliss</h1>
+            <div className="nav-links">
+                <Link to="/" className="nav-link-inner" >Home</Link>
+                <Link to="/shop" className="nav-link-inner">Shop</Link>
+                <Link to="/cart" className="nav-link-inner">Cart</Link>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
