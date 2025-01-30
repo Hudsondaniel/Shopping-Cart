06a2573852914ff0417e791c25fad61cdc012985
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2025-01-30 15:54:40 +0530

    Thought of using tailwind but nvm

--- ShoppingCart/src/components/Navbar.jsx
+++ ShoppingCart/src/components/Navbar.jsx
@@ -1,9 +1,8 @@
-// src/components/Navbar.jsx
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-600 text-white p-4 flex justify-between">
+    <nav className="flex ">
       <h1 className="text-xl font-bold">My Shop</h1>
       <div>
         <Link to="/" className="mr-4">Home</Link>
--- ShoppingCart/src/pages/Home.jsx
+++ ShoppingCart/src/pages/Home.jsx
@@ -6,7 +6,6 @@ export default function Home(){
     return (
         <div>
             <Navbar />
-            <h1>Home</h1>
         </div>
     )
 }
