592771f2666e6dd1c6ac4c101039087294dda7bf
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2025-02-03 21:38:26 +0530

    Pop up styling complete

--- ShoppingCart/src/components/PopUp.jsx
+++ ShoppingCart/src/components/PopUp.jsx
@@ -1,20 +1,23 @@
 import PropTypes from "prop-types";
-import '../CSS/PopUp.css'
+import "../CSS/PopUp.css";
 
-export default function PopUp({image, title, description, price, rating }){
-    return(
-        <div className="popup-container">
-            <div className="popup-image-container">
-                <img src={image} alt={title} className="popup-images" />
-            </div>
-            <div className="popup-details-container">
-                <h3 className="popup-title">{title}</h3>
-                <p className="popup-description">{description}</p>
-                <p className="popup-price">${price}</p>
-                <p className="popup-rating">⭐ {rating.rate} ({rating.count} reviews)</p>
+export default function PopUp({ image, title, description, price, rating, closePopup }) {
+    return (
+        <div className="popup-overlay" onClick={closePopup}>
+            <div className="popup-container" onClick={(e) => e.stopPropagation()}>
+                <button className="popup-close" onClick={closePopup}>&times;</button>
+                <div className="popup-image-container">
+                    <img src={image} alt={title} className="popup-images" />
+                </div>
+                <div className="popup-details-container">
+                    <h3 className="popup-title">{title}</h3>
+                    <p className="popup-description">{description}</p>
+                    <p className="popup-price">${price.toFixed(2)}</p>
+                    <p className="popup-rating">⭐ {rating.rate} ({rating.count} reviews)</p>
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 PopUp.propTypes = {
@@ -26,4 +29,5 @@ PopUp.propTypes = {
         rate: PropTypes.number,
         count: PropTypes.number,
     }).isRequired,
+    closePopup: PropTypes.func.isRequired,
 };
